fee69c9ee3c51ada8cd15dfa11586e605b9ef24f
GrimmOutlook <grimmoutlook87@gmail.com>
2015-07-17 00:23:37 -0400

    Modified navbar & added routes.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :categories
   resources :publishers
 
-  get 'about/index'
+  get 'about' => 'about#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
